2bf969f137be3ec0d2be04c5191df4c5af4d3641
Maarten Joosten <mjoosten@f0r5s19.codam.nl>
2022-03-04 17:39:10 +0100

    Rewrote to use ft_cmd

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mjoosten <mjoosten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 16:13:06 by mjoosten          #+#    #+#             */
-/*   Updated: 2022/03/04 12:55:00 by mjoosten         ###   ########.fr       */
+/*   Updated: 2022/03/04 17:38:29 by mjoosten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,42 @@
 
 int	main(int argc, char **argv, char **env)
 {
-	pid_t	pid;
 	char	**strs;
 	int		fds[2];
+	int		pipes[2];
 
 	if (argc < 5)
 		return (1);
-	pipe(fds);
+	pipe(pipes);
 	strs = ft_split(argv[2], ' ');
 	ft_getpath(strs, env);
-	pid = fork();
-	if (!pid)
-	{
-		dup2(open(argv[1], O_RDONLY), STDIN_FILENO);
-		dup2(fds[1], STDOUT_FILENO);
-		execve(*strs, strs, 0);
-	}
-	close(fds[1]);
-	waitpid(pid, 0, 0);
+	fds[0] = open(argv[1], O_RDONLY);
+	fds[1] = pipes[1];
+	ft_cmd(strs, fds, 1);
 	ft_free_array(strs);
 	strs = ft_split(argv[3], ' ');
 	ft_getpath(strs, env);
+	fds[0] = pipes[0];
+	fds[1] = open(argv[4], O_CREAT | O_WRONLY, 0666);
+	ft_cmd(strs, fds, 1);
+	ft_free_array(strs);
+	exit(EXIT_SUCCESS);
+}
+
+void	ft_cmd(char	**argv, int fds[2], int fd_close)
+{
+	pid_t	pid;
+
 	pid = fork();
 	if (!pid)
 	{
 		dup2(fds[0], STDIN_FILENO);
-		dup2(open(argv[4], O_CREAT | O_WRONLY, 0666), STDOUT_FILENO);
-		execve(*strs, strs, 0);
+		dup2(fds[1], STDOUT_FILENO);
+		execve(*argv, argv, 0);
+		ft_error(0);
 	}
-	close(fds[0]);
+	close(fds[fd_close]);
 	waitpid(pid, 0, 0);
-	ft_free_array(strs);
-	exit(EXIT_SUCCESS);
 }
 
 void	ft_getpath(char **strs, char **env)
@@ -57,7 +61,7 @@ void	ft_getpath(char **strs, char **env)
 	i = 0;
 	paths = ft_getpaths(env);
 	path = ft_strjoin(paths[i++], *strs);
-	while (access(path, X_OK))
+	while (access(path, F_OK))
 	{
 		free(path);
 		if (!paths[i])
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: mjoosten <mjoosten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 16:12:26 by mjoosten          #+#    #+#             */
-/*   Updated: 2022/03/04 12:35:06 by mjoosten         ###   ########.fr       */
+/*   Updated: 2022/03/04 17:29:30 by mjoosten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@
 
 char	**ft_getpaths(char **env);
 void	ft_getpath(char **strs, char **env);
-int		ft_exe(char *file, int fds[2]);
+void	ft_cmd(char	**argv, int fds[2], int close);
 
 #endif
